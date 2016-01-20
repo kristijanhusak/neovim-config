39af5a612c34a8514734cfb5112447faabf680a0
Kristijan Husak <husakkristijan@gmail.com>
2016-01-20 14:25:15 +0100

    Add pane numbers to status bar.

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #/bin/sh
-rm -rf ~/.config/nvim ~/.ackrc \
+rm -rf ~/.config/nvim ~/.ackrc ~/.tmux.conf ~/.zshrc \
 && curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim \
 && ln -s $(pwd)/init.vim ~/.config/nvim/init.vim \
 && ln -s $(pwd)/ackrc ~/.ackrc \
