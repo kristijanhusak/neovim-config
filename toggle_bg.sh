988ad8e9bda4fc54333278e02f8693216ce1d43e
Kristijan Husak <husakkristijan@gmail.com>
2020-07-14 10:45:46 +0200

    Use local kitty config file for toggling background color.

--- toggle_bg.sh
+++ toggle_bg.sh
@@ -2,11 +2,14 @@
 
 dark_colorscheme='~/.config/kitty/gruvbox.conf'
 light_colorscheme='~/.config/kitty/xcodelight.conf'
+local_path=~/.config/kitty/local.conf
 
 set_bg () {
   if [[ $1 = 'light' ]]; then
+    echo "env NVIM_COLORSCHEME_BG=light\ninclude $light_colorscheme" > $local_path
     kitty @ set-colors --all --configured $light_colorscheme
   else
+    echo "env NVIM_COLORSCHEME_BG=dark\ninclude $dark_colorscheme" > $local_path
     kitty @ set-colors --all --configured $dark_colorscheme
   fi
   export NVIM_COLORSCHEME_BG=$1
@@ -15,19 +18,15 @@ set_bg () {
 
 if [[ $1 = 'toggle' ]]; then
   if [[ $NVIM_COLORSCHEME_BG = 'light' ]]; then
-    sed -i 's/light/dark/g' ~/.config/kitty/colors.conf
-    sed -i "s@^include.*@include $dark_colorscheme@" ~/.config/kitty/colors.conf
     set_bg 'dark'
   else
-    sed -i 's/dark/light/g' ~/.config/kitty/colors.conf
-    sed -i "s@^include.*@include $light_colorscheme@" ~/.config/kitty/colors.conf
     set_bg 'light'
   fi
 else
-  current_bg='dark'
+  current_bg='light'
 
-  if [[ -z $(grep -P 'light' ~/.config/kitty/colors.conf) ]]; then
-    current_bg='light'
+  if [[ -f $local_path && ! -z $(grep 'dark' $local_path) ]]; then
+    current_bg='dark'
   fi
   set_bg $current_bg
 fi
