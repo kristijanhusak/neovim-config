ebb9b34050136800f884c71f704c3e6a328761b5
Kristijan Husak <husakkristijan@gmail.com>
2020-08-15 01:05:29 +0200

    Use polybar instead of i3blocks.

---
+++ i3/polybar/start.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+
+# Terminate already running bar instances
+killall -q polybar > /dev/null
+
+# Wait until the processes have been shut down
+while pgrep -x polybar >/dev/null; do sleep 1; done
+
+# Launch polybar
+polybar bar &
