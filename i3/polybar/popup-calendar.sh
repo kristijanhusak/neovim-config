a716cebafd7c28a10d57c4e9e8f7233af4fa6213
Kristijan Husak <husakkristijan@gmail.com>
2020-08-15 13:58:27 +0200

    Tweak calendar position.

--- i3/polybar/popup-calendar.sh
+++ i3/polybar/popup-calendar.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 BAR_HEIGHT=22  # polybar height
-BORDER_SIZE=1  # border size from your wm settings
+BORDER_SIZE=50  # border size from your wm settings
 YAD_WIDTH=222  # 222 is minimum possible value
 YAD_HEIGHT=193 # 193 is minimum possible value
 DATE="$(date +"%Y.%m.%d %H:%M")"
