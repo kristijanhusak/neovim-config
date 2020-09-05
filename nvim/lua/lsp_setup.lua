2bd18a36c883361a79072fcd097329f0bd685293
Kristijan Husak <husakkristijan@gmail.com>
2020-09-05 17:24:41 +0200

    Do not highlight current scope.

--- nvim/lua/lsp_setup.lua
+++ nvim/lua/lsp_setup.lua
@@ -2,9 +2,9 @@ local completion = require'completion'.on_attach
 local nvim_lsp = require'nvim_lsp'
 require'treesitter_setup'
 
-nvim_lsp.tsserver.setup{on_attach=on_attach}
-nvim_lsp.vimls.setup{on_attach=on_attach}
-nvim_lsp.intelephense.setup{on_attach=on_attach}
-nvim_lsp.gopls.setup{on_attach=on_attach}
-nvim_lsp.pyls.setup{on_attach=on_attach}
-nvim_lsp.sumneko_lua.setup{on_attach=on_attach}
+nvim_lsp.tsserver.setup{on_attach=completion}
+nvim_lsp.vimls.setup{on_attach=completion}
+nvim_lsp.intelephense.setup{on_attach=completion}
+nvim_lsp.gopls.setup{on_attach=completion}
+nvim_lsp.pyls.setup{on_attach=completion}
+nvim_lsp.sumneko_lua.setup{on_attach=completion}
--- nvim/lua/treesitter_setup.lua
+++ nvim/lua/treesitter_setup.lua
@@ -15,9 +15,6 @@ require'nvim-treesitter.configs'.setup {
     highlight_definitions = {
       enable = true
     },
-    highlight_current_scope = {
-      enable = true
-    },
     smart_rename = {
       enable = true,
       keymaps = {
