d820ac959a458089b60fc35076796d1f8d023e99
Kristijan Husak <kris@getsquire.com>
2020-12-30 12:21:41 +0100

    Fix edge colors for warnings and errors.

--- nvim/lua/partials/colorscheme.lua
+++ nvim/lua/partials/colorscheme.lua
@@ -4,7 +4,15 @@ vim.cmd [[augroup vimrc_colorscheme]]
   vim.cmd [[autocmd BufEnter * :syntax sync fromstart]]
   vim.cmd [[autocmd FileType dbout syn match dbout_null /(null)/ | hi link dbout_null Comment]]
   vim.cmd [[autocmd ColorScheme * hi QuickScopePrimary gui=bold,undercurl]]
+  vim.cmd [[autocmd ColorScheme edge call v:lua.kris.colorscheme.setup_edge()]]
 vim.cmd [[augroup END]]
+
+function _G.kris.colorscheme.setup_edge()
+  vim.cmd[[
+    hi WarningText guibg=NONE
+    hi ErrorText guibg=NONE
+  ]]
+end
 
 vim.o.termguicolors = true
 vim.o.background = vim.env.NVIM_COLORSCHEME_BG or 'dark'
@@ -14,7 +22,3 @@ vim.g.edge_sign_column_background = 'none'
 vim.cmd[[filetype plugin indent on]]
 vim.cmd[[syntax on]]
 vim.cmd('colorscheme '..(vim.env.NVIM_COLORSCHEME or 'edge'))
-vim.cmd[[
-  hi ErrorLine gui=undercurl
-  hi WarningLine gui=undercurl
-]]
