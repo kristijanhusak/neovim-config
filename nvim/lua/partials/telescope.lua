7a441bda64d687164eed3b2fa9abd78b3d511068
Kristijan Husak <husakkristijan@gmail.com>
2021-02-23 09:55:54 +0100

    Prioritize lsp document symbols over buffer tags.

--- nvim/lua/partials/telescope.lua
+++ nvim/lua/partials/telescope.lua
@@ -29,7 +29,7 @@ local transform_mod = require('telescope.actions.mt').transform_mod
 local custom_actions = transform_mod({
   jump_to_symbol = function(prompt_bufnr)
     actions.file_edit(prompt_bufnr)
-    builtin.current_buffer_tags()
+    builtin.lsp_document_symbols()
     vim.defer_fn(function()
       vim.cmd('startinsert')
     end, 0)
@@ -104,12 +104,12 @@ require'telescope'.setup({
 
 utils.keymap('n', '<C-p>', '<cmd>lua require("telescope.builtin").find_files({ find_command = { "rg", "--files" } })<cr>')
 utils.keymap('n', '<Leader>b', "<cmd>lua require('telescope.builtin').buffers({ sort_lastused = true })<cr>")
-utils.keymap('n', '<Leader>t', "<cmd>lua require('telescope.builtin').current_buffer_tags()<cr>")
+utils.keymap('n', '<Leader>t', "<cmd>lua require('telescope.builtin').lsp_document_symbols()<cr>")
 utils.keymap('n', '<Leader>m', "<cmd>call v:lua.kris.telescope.oldfiles_buffers()<cr>")
 utils.keymap('n', '<Leader>g', "<cmd>lua require('telescope.builtin').git_status()<cr>")
 
 utils.keymap('n', '<Leader>lu', "<cmd>lua require('telescope.builtin').lsp_references()<cr>")
 utils.keymap('n', '<Leader>lT', "<cmd>lua require('telescope.builtin').lsp_workspace_symbols()<cr>")
-utils.keymap('n', '<Leader>lt', "<cmd>lua require('telescope.builtin').lsp_document_symbols()<cr>")
+utils.keymap('n', '<Leader>lt', "<cmd>lua require('telescope.builtin').current_buffer_tags()<cr>")
 
 _G.kris.telescope = telescope
