86f7f7db70dc7a23c258fbe69a383a1f26f259a7
Kristijan Husak <kris@getsquire.com>
2021-06-04 13:17:46 +0200

    Load local nvimrc at the end to allow rewriting.

--- nvim/init.lua
+++ nvim/init.lua
@@ -14,3 +14,10 @@ require'partials/replace_pair'
 require'partials/search'
 require'partials/vimspector'
 require'partials/rg_source'
+
+-- Load .nvimrc manually until this PR is merged.
+-- https://github.com/neovim/neovim/pull/13503
+local local_vimrc = vim.fn.getcwd()..'/.nvimrc'
+if vim.loop.fs_stat(local_vimrc) then
+  vim.cmd('source '..local_vimrc)
+end
