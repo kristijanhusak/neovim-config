d257a4ec803b4e585281a4bd29af1481a3a4d872
Kristijan Husak <husakkristijan@gmail.com>
2021-06-05 15:05:40 +0200

    Remove lspsaga and use built in lsp + custom rename function.

--- nvim/lua/partials/lsp.lua
+++ nvim/lua/partials/lsp.lua
@@ -1,7 +1,6 @@
 local lsp = {}
 local nvim_lsp = require'lspconfig'
 local utils = require'partials/utils'
-local diagnostic = require'lspsaga.diagnostic'
 local last_completed_item = { menu = nil, word = nil }
 local ts_utils = require'nvim-treesitter/ts_utils'
 
@@ -27,7 +26,7 @@ end
 -- until https://github.com/neovim/neovim/issues/12587 is resolved
 function lsp.on_cursor_hold()
   if vim.fn.mode() ~= 'i' then
-    diagnostic.show_line_diagnostics()
+    vim.lsp.diagnostic.show_line_diagnostics({ border = 'single', show_header = false })
   end
 end
 
@@ -40,10 +39,9 @@ function lsp.tag_signature(word)
   end
 
   if not vim.tbl_isempty(content) then
-    table.insert(content, 1, 'Tag signature')
-    require'lspsaga.signaturehelp'.focusable_preview('tag_signature', function()
-      return content, vim.lsp.util.try_trim_markdown_code_blocks(content)
-    end)
+    vim.lsp.util.open_floating_preview(content, vim.lsp.util.try_trim_markdown_code_blocks(content), {
+      border = 'single',
+    })
     return true
   end
 
@@ -63,7 +61,27 @@ function lsp.signature_help()
     end
     if show_tag_signature then return end
   end
-  return require('lspsaga.signaturehelp').signature_help()
+  return vim.lsp.buf.signature_help()
+end
+
+function lsp.rename()
+  local current_val = vim.fn.expand('<cword>')
+  local bufnr, winnr = vim.lsp.util.open_floating_preview({ current_val }, '', {
+    border = 'single',
+    width = math.max(current_val:len() + 10, 30),
+  })
+  vim.api.nvim_set_current_win(winnr)
+  vim.api.nvim_buf_set_option(bufnr, 'modifiable', true)
+  vim.api.nvim_win_set_option(winnr, 'sidescrolloff', 0)
+  utils.buf_keymap(bufnr, 'i', '<CR>', '<cmd>lua kris.lsp.do_rename()<CR>')
+  vim.cmd[[startinsert!]]
+end
+
+function lsp.do_rename()
+  local new_name = vim.trim(vim.fn.getline('.'))
+  vim.api.nvim_win_close(0, true)
+  vim.api.nvim_feedkeys(utils.esc('<Esc>'), 'i', true)
+  vim.lsp.buf.rename(new_name)
 end
 
 nvim_lsp.diagnosticls.setup({
@@ -144,48 +162,49 @@ local luadev = require("lua-dev").setup({
 })
 nvim_lsp.sumneko_lua.setup(luadev)
 
-require'lspsaga'.init_lsp_saga({
-  rename_prompt_prefix = '',
-  error_sign = '',
-  warn_sign = '',
-  hint_sign = '',
-  infor_sign = '',
-  code_action_prompt = {
-    enable = true,
-    sign = false,
-    virtual_text = true,
-  },
-})
-
 vim.lsp.handlers['_typescript.rename'] = function(_, _, result)
   if not result then return end
   vim.fn.cursor(result.position.line + 1, result.position.character + 1)
-  vim.cmd [[:Lspsaga rename]]
+  vim.api.nvim_feedkeys(utils.esc('<Esc>'), 'v', true)
+  lsp.rename()
   return {}
 end
 
-vim.lsp.handlers["textDocument/publishDiagnostics"] = function(err, method, params, client_id, bufnr, config)
-  return vim.lsp.diagnostic.on_publish_diagnostics(
-    err, method, params, client_id, bufnr, vim.tbl_deep_extend("force", config or {}, { virtual_text = false })
-  )
-end
+vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(
+  vim.lsp.handlers.signature_help, { border = "single" }
+)
+
+vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
+  vim.lsp.handlers.hover, { border = "single" }
+)
+
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+  vim.lsp.diagnostic.on_publish_diagnostics, { virtual_text = false }
+)
+
+vim.cmd[[
+  sign define LspDiagnosticsSignError text= texthl=LspDiagnosticsSignError linehl= numhl=
+  sign define LspDiagnosticsSignWarning text= texthl=LspDiagnosticsSignWarning linehl= numhl=
+  sign define LspDiagnosticsSignInformation text= texthl=LspDiagnosticsSignInformation linehl= numhl=
+  sign define LspDiagnosticsSignHint text= texthl=LspDiagnosticsSignHint linehl= numhl=
+]]
+
 
 utils.keymap('n', '<leader>ld', '<cmd>lua vim.lsp.buf.definition()<CR>', { noremap = false })
 utils.keymap('n', '<leader>lc', '<cmd>lua vim.lsp.buf.declaration()<CR>', { noremap = false })
 utils.keymap('n', '<leader>lg', '<cmd>lua vim.lsp.buf.implementation()<CR>', { noremap = false })
-utils.keymap('n', '<leader>lh', ':Lspsaga hover_doc<CR>', { noremap = false })
+utils.keymap('n', '<leader>lh', '<cmd>lua vim.lsp.buf.hover()<CR>', { noremap = false })
 utils.keymap('n', '<leader>lH', '<cmd>lua kris.lsp.tag_signature(vim.fn.expand("<cword>"))<CR>')
-utils.keymap('n', '<leader>lp', ':Lspsaga preview_definition<CR>', { noremap = false })
 utils.keymap('n', '<leader>lf', '<cmd>lua vim.lsp.buf.formatting()<CR>', { noremap = false })
 utils.keymap('v', '<leader>lf', ':<C-u>call v:lua.vim.lsp.buf.range_formatting()<CR>', { noremap = false })
 utils.keymap('n', '<leader>li', '<cmd>lua vim.lsp.buf.incoming_calls()<CR>', { noremap = false })
 utils.keymap('n', '<leader>lo', '<cmd>lua vim.lsp.buf.outgoing_calls()<CR>', { noremap = false })
-utils.keymap('n', '<leader>le', '<cmd>lua require"lspsaga.diagnostic".show_line_diagnostics()<CR>', { noremap = false })
+utils.keymap('n', '<leader>le', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', { noremap = false })
 utils.keymap('n', '<Leader>e', ':LspTroubleToggle lsp_document_diagnostics<CR>')
-utils.keymap('n', '<leader>lr', ':Lspsaga rename<CR>', { noremap = false })
-utils.keymap('n', '[g', ':Lspsaga diagnostic_jump_prev<CR>')
-utils.keymap('n', ']g', ':Lspsaga diagnostic_jump_next<CR>')
-utils.keymap('n', '<Leader>la', ':Lspsaga code_action<CR>')
-utils.keymap('v', '<Leader>la', ':<C-U>Lspsaga range_code_action<CR>')
+utils.keymap('n', '<leader>lr', '<cmd>lua kris.lsp.rename()<CR>', { noremap = false })
+utils.keymap('n', '[g', '<cmd>lua vim.lsp.diagnostic.got_prev()<CR>')
+utils.keymap('n', ']g', '<cmd>lua vim.lsp.diagnostic.got_next()<CR>')
+utils.keymap('n', '<Leader>la', '<cmd>lua vim.lsp.buf.code_action()<CR>')
+utils.keymap('v', '<Leader>la', '<cmd>lua vim.lsp.buf.range_code_action()<CR>')
 
 _G.kris.lsp = lsp
