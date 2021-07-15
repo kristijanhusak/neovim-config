5270d4a5fc12f18d679523d9b1e4105c1893c029
Kristijan Husak <husakkristijan@gmail.com>
2021-07-15 23:42:41 +0200

    Update cron notifier function.

--- nvim/lua/partials/orgmode_config.lua
+++ nvim/lua/partials/orgmode_config.lua
@@ -6,11 +6,17 @@ return {
     reminder_time = {0, 1, 5, 10},
     repeater_reminder_time = {0, 1, 5, 10},
     deadline_warning_reminder_time = { 0 },
-    cron_notifier = function(task)
-    return vim.loop.spawn('notify-send', { args = {
-      '--icon=/home/kristijan/.config/nvim/pack/packager/start/orgmode.nvim/assets/orgmode_nvim.png',
-      string.format('%s (in %d min.)\n%s\n%s: %s', task.category, task.minutes, task.line, task.type, task.time:to_string()),
-      }})
-  end
+    cron_notifier = function(tasks)
+      for _, task in ipairs(tasks) do
+        local title = string.format('%s (in %d min.)', task.category, task.minutes)
+        local subtitle = string.format('%s %s %s', string.rep('*', task.level), task.todo, task.title)
+        local date = string.format('%s: %s', task.type, task.time:to_string())
+
+        vim.loop.spawn('notify-send', { args = {
+          '--icon=/home/kristijan/.config/nvim/pack/packager/start/orgmode.nvim/assets/orgmode_nvim.png',
+          string.format('%s\n%s\n%s', title, subtitle, date),
+        }})
+      end
+    end
   }
 }
