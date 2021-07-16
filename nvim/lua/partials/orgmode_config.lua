83788cb579ec000eaea19fd3d5f94a4294d197ad
Kristijan Husak <kris@getsquire.com>
2021-07-16 11:39:26 +0200

    Update cron notifier function.

--- nvim/lua/partials/orgmode_config.lua
+++ nvim/lua/partials/orgmode_config.lua
@@ -1,21 +1,22 @@
 return {
   org_agenda_files = '~/Dropbox/org/*',
   org_default_notes_file = '~/Dropbox/org/refile.org',
-  -- Experimental settings. Currently available only on feature/notifications branch of orgmode
   notifications = {
     reminder_time = {0, 1, 5, 10},
     repeater_reminder_time = {0, 1, 5, 10},
     deadline_warning_reminder_time = { 0 },
     cron_notifier = function(tasks)
       for _, task in ipairs(tasks) do
-        local title = string.format('%s (in %d min.)', task.category, task.minutes)
+        local title = string.format('%s (%s)', task.category, task.humanized_duration)
         local subtitle = string.format('%s %s %s', string.rep('*', task.level), task.todo, task.title)
         local date = string.format('%s: %s', task.type, task.time:to_string())
 
-        vim.loop.spawn('notify-send', { args = {
-          '--icon=/home/kristijan/.config/nvim/pack/packager/start/orgmode.nvim/assets/orgmode_nvim.png',
-          string.format('%s\n%s\n%s', title, subtitle, date),
-        }})
+        if vim.fn.executable('notify-send') then
+          vim.loop.spawn('notify-send', { args = {
+            '--icon=/home/kristijan/.config/nvim/pack/packager/start/orgmode.nvim/assets/orgmode_nvim.png',
+            string.format('%s\n%s\n%s', title, subtitle, date),
+          }})
+        end
       end
     end
   }
