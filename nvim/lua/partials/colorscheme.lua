95d7bb1ee6d02782f57e98b79bb02f9606762766
Kristijan Husak <husakkristijan@gmail.com>
2021-07-23 19:02:56 +0200

    Update org agenda day highlight.

--- nvim/lua/partials/colorscheme.lua
+++ nvim/lua/partials/colorscheme.lua
@@ -20,6 +20,7 @@ require('github-theme').setup({
 vim.cmd(string.format('hi StatusLineNC guibg=%s', colors[bg_statusline]))
 vim.cmd(string.format('hi OrgDONE guifg=%s gui=bold', colors.git.add))
 vim.cmd(string.format('hi OrgAgendaScheduled guifg=%s', colors.green))
+vim.cmd('hi link OrgAgendaDay Directory')
 vim.defer_fn(function()
   vim.g.terminal_color_2 = colors.gitSigns.add
 end, 1)
