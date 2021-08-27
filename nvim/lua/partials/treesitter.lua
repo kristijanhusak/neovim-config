662378f10719de200c7278961f7edd585bdf2702
Kristijan Husak <husakkristijan@gmail.com>
2021-08-27 08:44:19 +0200

    Add nvim-playground and set up org treesitter parser.

--- nvim/lua/partials/treesitter.lua
+++ nvim/lua/partials/treesitter.lua
@@ -1,6 +1,25 @@
 require'nvim-treesitter.configs'.setup {
   highlight = {
     enable = true,
+    additional_vim_regex_highlighting = {'org'},
+  },
+  playground = {
+    enable = true,
+    disable = {},
+    updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
+    persist_queries = false, -- Whether the query persists across vim sessions
+    keybindings = {
+      toggle_query_editor = 'o',
+      toggle_hl_groups = 'i',
+      toggle_injected_languages = 't',
+      toggle_anonymous_nodes = 'a',
+      toggle_language_display = 'I',
+      focus_language = 'f',
+      unfocus_language = 'F',
+      update = 'R',
+      goto_node = '<cr>',
+      show_help = '?',
+    },
   },
   incremental_selection = {
     enable = true,
@@ -85,4 +104,13 @@ require'nvim-treesitter.configs'.setup {
     }
   },
   ensure_installed = {"javascript", "typescript", "php", "go", "python", "lua", "jsdoc"}
+}
+
+local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
+parser_config.org = {
+  install_info = {
+    url = '~/github/tree-sitter-org',
+    files = {'src/parser.c', 'src/scanner.cc'},
+  },
+  filetype = 'org',
 }
