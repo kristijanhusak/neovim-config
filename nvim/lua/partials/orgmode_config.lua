2054dad41879b4c41e54b1d2b5280d05bc58c9db
Kristijan Husak <husakkristijan@gmail.com>
2021-10-15 21:37:38 +0200

    Enable searching archive files with orgmode.

--- nvim/lua/partials/orgmode_config.lua
+++ nvim/lua/partials/orgmode_config.lua
@@ -2,6 +2,7 @@ return {
   org_agenda_files = '~/Dropbox/org/*',
   org_default_notes_file = '~/Dropbox/org/refile.org',
   org_hide_emphasis_markers = true,
+  org_agenda_text_search_extra_files = {'agenda-archives'},
   org_agenda_start_on_weekday = false,
   org_todo_keywords = {'TODO(t)', 'PROGRESS(p)', '|', 'DONE(d)', 'REJECTED(r)'},
   org_agenda_templates = {
