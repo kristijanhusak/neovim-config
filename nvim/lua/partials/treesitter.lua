e8b08183f97bfaf7dd94a5aebd9fed295914ce0a
Kristijan Husak <husakkristijan@gmail.com>
2021-12-08 16:06:50 +0100

    Use ts org parser from original repo instead of local.

--- nvim/lua/partials/treesitter.lua
+++ nvim/lua/partials/treesitter.lua
@@ -1,8 +1,9 @@
 local parser_config = require('nvim-treesitter.parsers').get_parser_configs()
 parser_config.org = {
   install_info = {
-    url = '~/github/tree-sitter-org',
-    files = { 'src/parser.c', 'src/scanner.cc' },
+    url = 'https://github.com/milisims/tree-sitter-org',
+    revision = 'main',
+    files = {'src/parser.c', 'src/scanner.cc'},
   },
   filetype = 'org',
 }
