35f0035b35e9805fde8173d846814255aa082f8e
Kristijan Husak <husakkristijan@gmail.com>
2022-02-06 16:59:25 +0100

    Use dedicated orgmode function to load TS grammar.

--- nvim/lua/partials/treesitter.lua
+++ nvim/lua/partials/treesitter.lua
@@ -1,12 +1,4 @@
-local parser_config = require('nvim-treesitter.parsers').get_parser_configs()
-parser_config.org = {
-  install_info = {
-    url = 'https://github.com/milisims/tree-sitter-org',
-    revision = 'main',
-    files = { 'src/parser.c', 'src/scanner.cc' },
-  },
-  filetype = 'org',
-}
+require('orgmode').setup_ts_grammar()
 
 require('nvim-treesitter.configs').setup({
   highlight = {
