3c2fb542f7b8e17c40b4d40e04ed6e7180c1aeac
Kristijan Husak <husakkristijan@gmail.com>
2022-02-16 21:31:08 +0100

    Test new nvim-tree configuration.

--- nvim/lua/partials/plugins.lua
+++ nvim/lua/partials/plugins.lua
@@ -62,13 +62,6 @@ vim.cmd([[augroup END]])
 
 function plugins.handle_vimenter()
   vim.g.vsnip_snippet_dir = vim.fn.fnamemodify(vim.env.MYVIMRC, ':p:h') .. '/snippets'
-   local stats = vim.loop.fs_stat(vim.fn.expand('%:p'))
-   if not stats then
-    vim.cmd[[NvimTreeToggle]]
-    vim.schedule(function()
-      vim.cmd[[wincmd p]]
-    end)
-   end
 end
 
 vim.keymap.set('n', '<Leader><space>', ':noh<CR>')
@@ -131,6 +124,8 @@ vim.g.nvim_tree_icons = {
 vim.g.nvim_tree_git_hl = 1
 
 require('nvim-tree').setup({
+  disable_netrw = true,
+  open_on_setup = true,
   update_focused_file = {
     enable = true,
   },
