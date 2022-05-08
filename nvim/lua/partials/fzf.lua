0c927f3d0a8cd4eb4da3970d9f15f86201012445
Kristijan Husak <husakkristijan@gmail.com>
2022-05-08 16:32:04 +0200

    Wait for lsp to load when running custom lsp tags fzf

--- nvim/lua/partials/fzf.lua
+++ nvim/lua/partials/fzf.lua
@@ -12,6 +12,11 @@ end
 
 local function show_lsp_tags(selected)
   return run_cmd(selected, function()
+    vim.wait(5000, function()
+      return #vim.tbl_filter(function(client)
+        return client.resolved_capabilities.document_symbol
+      end, vim.lsp.get_active_clients()) > 0
+    end)
     return fzf.lsp_document_symbols()
   end)
 end
