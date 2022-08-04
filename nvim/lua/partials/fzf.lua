3ebbaacfa4051ed0c8ac7e5acd3e138b8e8c6e06
Kristijan Husak <husakkristijan@gmail.com>
2022-08-04 16:09:51 +0200

    Use 0.7.2 compatible settings for lsp server capabilities

--- nvim/lua/partials/fzf.lua
+++ nvim/lua/partials/fzf.lua
@@ -15,7 +15,7 @@ local function show_lsp_tags(selected)
   return run_cmd(selected, function()
     vim.wait(5000, function()
       return #vim.tbl_filter(function(client)
-        return client.server_capabilities.document_symbol
+        return client.resolved_capabilities.document_symbol
       end, vim.lsp.get_active_clients()) > 0
     end)
     return fzf.lsp_document_symbols()
