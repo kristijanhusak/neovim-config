0dd49ebe3f61651e132cce0a6567836040991453
Kristijan Husak <husakkristijan@gmail.com>
2022-08-05 21:12:06 +0200

    Add vim ui window title prompt

--- nvim/lua/partials/ui.lua
+++ nvim/lua/partials/ui.lua
@@ -4,6 +4,30 @@ local ui = {
   last_input = {},
 }
 
+local function add_title_to_win(winnr, bufnr, title)
+  vim.cmd([[redraw]])
+  local title_bufnr = vim.api.nvim_create_buf(false, true)
+  vim.api.nvim_buf_set_lines(title_bufnr, 0, -1, true, { (' %s'):format(vim.trim(title)) })
+  local title_winnr = vim.api.nvim_open_win(title_bufnr, false, {
+    relative = 'win',
+    win = winnr,
+    width = vim.trim(title):len() + 2,
+    height = 1,
+    row = -1,
+    col = 1,
+    focusable = false,
+    zindex = 151,
+    style = 'minimal',
+    noautocmd = true,
+  })
+  vim.api.nvim_create_autocmd('WinClosed', {
+    buffer = bufnr,
+    callback = function()
+      vim.api.nvim_win_close(title_winnr, true)
+    end,
+  })
+end
+
 vim.ui.select = function(items, opts, on_choice)
   vim.validate({
     items = { items, 'table', false },
@@ -13,7 +37,7 @@ vim.ui.select = function(items, opts, on_choice)
   local choices = {}
   local format_item = opts.format_item or tostring
   for i, item in pairs(items) do
-    table.insert(choices, string.format('%d: %s', i, format_item(item)))
+    table.insert(choices, string.format('%d. %s ', i, format_item(item)))
   end
 
   kris.ui.last_select = {
@@ -24,6 +48,11 @@ vim.ui.select = function(items, opts, on_choice)
   local bufnr, winnr = vim.lsp.util.open_floating_preview(choices, '', {
     border = 'rounded',
   })
+
+  if opts.prompt then
+    add_title_to_win(winnr, bufnr, opts.prompt)
+  end
+
   vim.api.nvim_set_current_win(winnr)
   vim.keymap.set('n', '<CR>', kris.ui.on_select, { buffer = bufnr })
 end
@@ -39,6 +68,11 @@ vim.ui.input = function(opts, on_confirm)
     width = math.max(current_val:len() + 10, 30),
     wrap = false,
   })
+
+  if opts.prompt then
+    add_title_to_win(winnr, bufnr, opts.prompt)
+  end
+
   ui.last_input = {
     val = current_val,
     on_confirm = on_confirm,
