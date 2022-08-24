b653da062028b94c0ad09b002943396af4da24e3
Kristijan Husak <husakkristijan@gmail.com>
2022-08-24 10:16:12 +0200

    Remove some system level apps from autostart

--- bspwm/autostart.sh
+++ bspwm/autostart.sh
@@ -1,9 +1,6 @@
-/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 xautolock -time 20 -locker blurlock &
 ~/.config/polybar/start.sh
 kitty &
 google-chrome-stable &
 volumeicon &
 dropbox &
-pamac-tray &
-nitrogen --restore &
