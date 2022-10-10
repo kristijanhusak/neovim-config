ae8d750806892a7e92f88cb25226aa8089d889a6
Kristijan Husak <husakkristijan@gmail.com>
2022-10-10 13:01:50 +0200

    Extract asterisk into a plugin file

--- nvim/lua/partials/plugins.lua
+++ nvim/lua/partials/plugins.lua
@@ -50,59 +50,14 @@ require('packager').setup(function(packager)
   packager.add('AndrewRadev/splitjoin.vim')
   packager.add('nvim-lua/plenary.nvim')
   packager.add('ludovicchabant/vim-gutentags')
-  packager.add('andymass/vim-matchup')
-  packager.add('haya14busa/vim-asterisk')
   packager.add('stefandtw/quickfix-reflector.vim')
   packager.add('wakatime/vim-wakatime')
-  packager.add('hrsh7th/vim-vsnip')
   packager.add('Raimondi/delimitMate')
   packager.add('lewis6991/impatient.nvim')
   packager.add('antoinemadec/FixCursorHold.nvim')
   packager.add('https://gitlab.com/yorickpeterse/nvim-pqf')
-  packager.add('github/copilot.vim')
-  packager.add('sindrets/diffview.nvim')
 end)
 
-vim.g.vsnip_snippet_dir = vim.fn.fnamemodify(vim.env.MYVIMRC, ':p:h') .. '/snippets'
-
-vim.keymap.set('n', 'n', 'nzz')
-vim.keymap.set('n', 'N', 'Nzz')
-vim.keymap.set('', '*', '<Plug>(asterisk-z*)')
-vim.keymap.set('', '#', '<Plug>(asterisk-z#)')
-vim.keymap.set('', 'g*', '<Plug>(asterisk-gz*)')
-vim.keymap.set('', 'g#', '<Plug>(asterisk-gz#)')
-
-vim.keymap.set('n', '<Leader>G', ':vert G<CR>', { silent = true })
-
-vim.keymap.set('n', '<leader>do', function()
-  if vim.t.diffview_view_initialized then
-    return vim.cmd('DiffviewClose')
-  end
-  return vim.cmd('DiffviewOpen')
-end, { silent = true })
-vim.keymap.set('n', '<leader>dh', ':DiffviewFileHistory %<CR>', { silent = true })
-vim.keymap.set('n', '<leader>dc', function()
-  vim.cmd([[DiffviewClose]])
-  vim.cmd([[botright Git commit]])
-end, { silent = true })
-vim.keymap.set('n', '<leader>da', function()
-  vim.cmd([[DiffviewClose]])
-  vim.cmd([[botright Git commit --amend]])
-end, { silent = true })
-vim.keymap.set('n', '<leader>dp', ':Git push<CR>', { silent = true })
-vim.keymap.set('n', '<leader>df', ':Git push --force-with-lease<CR>', { silent = true })
-
-vim.g.matchup_matchparen_status_offscreen = 0
-vim.g.matchup_matchparen_nomode = 'ivV'
-vim.g.matchup_matchparen_deferred = 100
-
-vim.g.vsnip_filetypes = {
-  typescript = { 'javascript' },
-  typescriptreact = { 'javascript' },
-  javascriptreact = { 'javascript' },
-}
-
-vim.g.js_file_import_use_telescope = 1
 require('partials.plugins.custom')
 
 vim.g.delimitMate_expand_cr = 1
