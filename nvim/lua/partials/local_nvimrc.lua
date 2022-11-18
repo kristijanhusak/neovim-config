ea31304a2b6286942f62eec52a1205583b391ad2
Kristijan Husak <husakkristijan@gmail.com>
2022-11-18 13:01:47 +0100

    Properly source local vimrc if confirmed

--- nvim/lua/partials/local_nvimrc.lua
+++ nvim/lua/partials/local_nvimrc.lua
@@ -1,8 +1,10 @@
 local local_vimrc = vim.fn.getcwd() .. '/.nvimrc'
 if vim.loop.fs_stat(local_vimrc) then
+  local source = true
   if vim.secure then
-    vim.secure.read(local_vimrc)
-  else
+    source = vim.secure.read(local_vimrc)
+  end
+  if source then
     vim.cmd.source(local_vimrc)
   end
 end
