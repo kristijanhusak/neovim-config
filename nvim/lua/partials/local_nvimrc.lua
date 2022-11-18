0a4cbf37aafef33b8084e3a2062f40cf700d170f
Kristijan Husak <husakkristijan@gmail.com>
2022-11-18 10:39:30 +0100

    Use vim.secure lua module to read project level configuration if available

--- nvim/lua/partials/local_nvimrc.lua
+++ nvim/lua/partials/local_nvimrc.lua
@@ -1,4 +1,8 @@
 local local_vimrc = vim.fn.getcwd() .. '/.nvimrc'
 if vim.loop.fs_stat(local_vimrc) then
-  vim.cmd.source(local_vimrc)
+  if vim.secure then
+    vim.secure.read(local_vimrc)
+  else
+    vim.cmd.source(local_vimrc)
+  end
 end
