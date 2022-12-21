41ff8bded5afa464838a5ba36b434bac69f87ded
Kristijan Husak <husakkristijan@gmail.com>
2022-12-21 13:35:29 +0100

    Use init instead of config for plugins where we set global variables

--- nvim/lua/partials/plugins/matchup.lua
+++ nvim/lua/partials/plugins/matchup.lua
@@ -2,7 +2,7 @@ local matchup = {
   'andymass/vim-matchup',
   event = 'VeryLazy',
 }
-matchup.config = function()
+matchup.init = function()
   vim.g.matchup_matchparen_status_offscreen = 0
   vim.g.matchup_matchparen_nomode = 'ivV'
   vim.g.matchup_matchparen_deferred = 100
--- nvim/lua/partials/plugins/snippets.lua
+++ nvim/lua/partials/plugins/snippets.lua
@@ -2,7 +2,7 @@ local snippets = {
   'hrsh7th/vim-vsnip',
   event = 'VeryLazy',
 }
-snippets.config = function()
+snippets.init = function()
   vim.g.vsnip_snippet_dir = vim.fn.fnamemodify(vim.env.MYVIMRC, ':p:h') .. '/snippets'
   vim.g.vsnip_filetypes = {
     typescript = { 'javascript' },
