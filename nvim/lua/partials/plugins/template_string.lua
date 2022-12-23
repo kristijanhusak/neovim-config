4e39b316092d7182b2c2ad38eb4e1684cf3eb5d0
Kristijan Husak <husakkristijan@gmail.com>
2022-12-23 16:14:30 +0100

    Re-trigger filetype event after template string plugin is loaded to properly attach it

--- nvim/lua/partials/plugins/template_string.lua
+++ nvim/lua/partials/plugins/template_string.lua
@@ -1,11 +1,17 @@
+local filetypes = { 'javascript', 'javascriptreact', 'typescript', 'typescriptreact' }
+
 local template_string = {
   'axelvc/template-string.nvim',
-  ft = { 'javascript', 'javascriptreact', 'typescript', 'typescriptreact' },
+  ft = filetypes,
 }
 template_string.config = function()
   require('template-string').setup({
     remove_template_string = true,
   })
+
+  if vim.tbl_contains(filetypes, vim.bo.filetype) then
+    vim.cmd('doautocmd FileType '..vim.bo.filetype)
+  end
 end
 
 return template_string
