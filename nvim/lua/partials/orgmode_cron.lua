bc56236e7d3203e8bc365b4274c7044c8bad7694
Kristijan Husak <husakkristijan@gmail.com>
2022-12-27 11:02:31 +0100

    Disable warning for different requires in orgmode cron

--- nvim/lua/partials/orgmode_cron.lua
+++ nvim/lua/partials/orgmode_cron.lua
@@ -3,4 +3,5 @@ local orgmode = vim.fn.stdpath('data') .. '/lazy/orgmode'
 vim.opt.runtimepath:append(treesitter)
 vim.opt.runtimepath:append(orgmode)
 
+---@diagnostic disable-next-line: different-requires
 require('orgmode').cron(require('partials.plugins.orgmode').orgmode_config)
