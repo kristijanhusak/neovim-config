d0550af3c0b95ae5e41d91e459548db17f9ab1e7
Kristijan Husak <husakkristijan@gmail.com>
2023-04-16 20:52:19 +0200

    Add firenvim and disable statusline in firenvim

--- nvim/lua/partials/plugins.lua
+++ nvim/lua/partials/plugins.lua
@@ -8,4 +8,13 @@ return {
   { 'stefandtw/quickfix-reflector.vim', event = 'VeryLazy' },
   { 'wakatime/vim-wakatime', event = 'VeryLazy' },
   { 'LunarVim/bigfile.nvim' },
+  {
+    'glacambre/firenvim',
+
+    cond = not not vim.g.started_by_firenvim,
+    build = function()
+      require('lazy').load({ plugins = 'firenvim', wait = true })
+      vim.fn['firenvim#install'](0)
+    end,
+  },
 }
