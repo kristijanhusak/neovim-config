905f6d7cebd92abb7765bc39a4aa05eb83083f23
Kristijan Husak <husakkristijan@gmail.com>
2023-04-17 11:07:45 +0200

    Disable copilot in firenvim

--- nvim/lua/partials/plugins/copilot.lua
+++ nvim/lua/partials/plugins/copilot.lua
@@ -1,6 +1,9 @@
 return {
   'zbirenbaum/copilot.lua',
   event = 'InsertEnter',
+  enabled = function()
+    return not vim.g.started_by_firenvim
+  end,
   opts = {
     panel = {
       enabled = false,
