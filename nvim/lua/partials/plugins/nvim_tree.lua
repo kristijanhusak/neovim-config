72a9bfd27ca1be2156e0f7094e7c27188c5fe2a0
Kristijan Husak <husakkristijan@gmail.com>
2023-04-18 11:24:25 +0200

    Migrate nvim tree migration to on_attach

--- nvim/lua/partials/plugins/nvim_tree.lua
+++ nvim/lua/partials/plugins/nvim_tree.lua
@@ -11,6 +11,15 @@ end
 
 nvim_tree.opts = {
   hijack_unnamed_buffer_when_opening = false,
+  on_attach = function(bufnr)
+    local api = require('nvim-tree.api')
+    local opts = { buffer = bufnr, noremap = true, silent = true, nowait = true }
+    api.config.mappings.default_on_attach(bufnr)
+
+    vim.keymap.set('n', 's', api.node.open.vertical, opts)
+    vim.keymap.set('n', '<s-c>', api.tree.change_root_to_node, opts)
+    vim.keymap.set('n', '<s-x>', api.node.run.system, opts)
+  end,
   disable_netrw = true,
   update_focused_file = {
     enable = true,
@@ -36,13 +45,6 @@ nvim_tree.opts = {
           col = 0,
         }
       end,
-    },
-    mappings = {
-      list = {
-        { key = { 's' }, action = 'vsplit' },
-        { key = { '<s-c>' }, action = 'cd' },
-        { key = { '<s-x>' }, action = 'system_open' },
-      },
     },
   },
   renderer = {
