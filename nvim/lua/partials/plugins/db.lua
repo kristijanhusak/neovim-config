1e5dce899e741896956064008b38e3cac7477207
Kristijan Husak <husakkristijan@gmail.com>
2023-09-15 14:46:38 +0200

    Use nvim-notify for dadbod ui

--- nvim/lua/partials/plugins/db.lua
+++ nvim/lua/partials/plugins/db.lua
@@ -15,6 +15,7 @@ return {
     vim.g.db_ui_show_help = 0
     vim.g.db_ui_win_position = 'right'
     vim.g.db_ui_use_nerd_fonts = 1
+    vim.g.db_ui_use_nvim_notify = 1
 
     vim.g.db_ui_save_location = '~/Dropbox/dbui'
     vim.g.db_ui_tmp_query_location = '~/code/queries'
