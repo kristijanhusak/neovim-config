4270694a5294cea55b545026a78caef030157c66
Kristijan Husak <husakkristijan@gmail.com>
2023-09-15 11:50:42 +0200

    Update dbui config to return immediatelly

--- nvim/lua/partials/plugins/db.lua
+++ nvim/lua/partials/plugins/db.lua
@@ -1,22 +1,24 @@
-local db = {
+return {
   'kristijanhusak/vim-dadbod-ui',
   dependencies = {
     { 'tpope/vim-dadbod', lazy = true },
     { 'kristijanhusak/vim-dadbod-completion', ft = { 'sql', 'mysql', 'plsql' }, lazy = true },
   },
-  cmd = { 'DBUI', 'DBUIFindBuffer' },
+  event = 'VeryLazy',
+  cmd = {
+    'DBUI',
+    'DBUIToggle',
+    'DBUIAddConnection',
+    'DBUIFindBuffer',
+  },
+  init = function()
+    vim.g.db_ui_show_help = 0
+    vim.g.db_ui_win_position = 'right'
+    vim.g.db_ui_use_nerd_fonts = 1
+
+    vim.g.db_ui_save_location = '~/Dropbox/dbui'
+    vim.g.db_ui_tmp_query_location = '~/code/queries'
+
+    vim.g.db_ui_hide_schemas = { 'pg_toast_temp.*' }
+  end,
 }
-db.init = function()
-  vim.g.db_ui_show_help = 0
-  vim.g.db_ui_win_position = 'right'
-  vim.g.db_ui_use_nerd_fonts = 1
-
-  vim.g.db_ui_save_location = '~/Dropbox/dbui'
-  vim.g.db_ui_tmp_query_location = '~/code/queries'
-
-  vim.g.db_ui_hide_schemas = { 'pg_toast_temp.*' }
-
-  return db
-end
-
-return db
