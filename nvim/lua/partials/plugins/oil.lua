b488d2d83387aae175c842a673842c0b0f04ad8a
Kristijan Husak <husakkristijan@gmail.com>
2023-09-21 11:27:19 +0200

    Test oil.nvim plugin

---
+++ nvim/lua/partials/plugins/oil.lua
@@ -0,0 +1,16 @@
+return {
+  'stevearc/oil.nvim',
+  opts = {
+    keymaps = {
+      ['<C-v>'] = 'actions.select_vsplit',
+      o = 'actions.select',
+      q = 'actions.close',
+      Q = 'actions.close',
+    },
+  },
+  cmd = {'Oil'},
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  init = function()
+    vim.keymap.set('n', '-', '<CMD>Oil<CR>')
+  end,
+}
