ec1aaa151300bf4979af5b549742395778180319
Kristijan Husak <husakkristijan@gmail.com>
2023-09-23 19:08:52 +0200

    test indent blankline v3

--- nvim/lua/partials/plugins/indent_blankline.lua
+++ nvim/lua/partials/plugins/indent_blankline.lua
@@ -1,10 +1,52 @@
 return {
   'lukas-reineke/indent-blankline.nvim',
+  branch = 'v3',
   event = 'VeryLazy',
-  opts = {
-    char = '▏',
-    context_char = '▏',
-    show_current_context = true,
-    disable_with_nolist = true,
-  },
+  config = function()
+    require('ibl').setup({
+      indent = {
+        char = '▏',
+        highlight = 'IndentBlanklineChar',
+      },
+      scope = {
+        highlight = 'IndentBlanklineContextChar',
+        show_start = false,
+        show_end = false,
+        include = {
+          node_type = {
+            lua = {
+              'chunk',
+              'do_statement',
+              'while_statement',
+              'repeat_statement',
+              'if_statement',
+              'for_statement',
+              'function_declaration',
+              'function_definition',
+              'table_constructor',
+              'assignment_statement',
+            },
+            typescript = {
+              'statement_block',
+              'function',
+              'arrow_function',
+              'function_declaration',
+              'method_definition',
+              'for_statement',
+              'for_in_statement',
+              'catch_clause',
+              'object_pattern',
+              'arguments',
+              'switch_case',
+              'switch_statement',
+              'switch_default',
+              'object',
+              'object_type',
+              'ternary_expression',
+            },
+          },
+        },
+      },
+    })
+  end,
 }
