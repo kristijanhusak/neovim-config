06f67aa47368e8ec9106307e314b0fe018dcf4b9
Kristijan Husak <husakkristijan@gmail.com>
2023-11-06 15:32:33 +0100

    Add note when switching projects

--- nvim/lua/partials/plugins/workspaces.lua
+++ nvim/lua/partials/plugins/workspaces.lua
@@ -2,6 +2,11 @@ return {
   'natecraddock/workspaces.nvim',
   cmd = { 'WorkspacesAdd', 'WorkspacesOpen' },
   opts = {
-    path = vim.fs.normalize('~/Dropbox/workspaces')
+    path = vim.fs.normalize('~/Dropbox/workspaces'),
+    hooks = {
+      open = function()
+        vim.notify((' Switched to project: %s '):format(vim.fn.getcwd()))
+      end,
+    },
   },
 }
