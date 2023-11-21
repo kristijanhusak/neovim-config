d00e7f9a38e969b1361a04dfacbbeb95d27055c0
Kristijan Husak <husakkristijan@gmail.com>
2023-11-21 20:04:04 +0100

    fix gitlinker to use command

--- nvim/lua/partials/plugins/gitlinker.lua
+++ nvim/lua/partials/plugins/gitlinker.lua
@@ -1,17 +1,16 @@
 local gitlinker = {
   'linrongbin16/gitlinker.nvim',
+  cmd = { 'GitLink' },
 }
 
 gitlinker.init = function()
   vim.keymap.set({ 'n', 'v' }, '<leader>yg', function()
-    require('gitlinker').link({ action = require('gitlinker.actions').system })
+    vim.cmd('GitLink!')
   end, { silent = true })
 end
 
 gitlinker.config = function()
-  require('gitlinker').setup({
-    mapping = {},
-  })
+  require('gitlinker').setup()
 end
 
 return gitlinker
