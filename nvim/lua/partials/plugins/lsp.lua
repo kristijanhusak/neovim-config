addd21163332521f7eb9d9ba4e1563a7a02eaa47
Kristijan Husak <husakkristijan@gmail.com>
2024-01-30 16:23:16 +0100

    Remove some lua globals

--- nvim/lua/partials/plugins/lsp.lua
+++ nvim/lua/partials/plugins/lsp.lua
@@ -226,7 +226,7 @@ function setup.servers()
     settings = {
       Lua = {
         diagnostics = {
-          globals = { 'vim', 'describe', 'it', 'before_each', 'after_each', 'assert' },
+          globals = { 'vim' },
         },
         workspace = {
           checkThirdParty = 'Disable',
