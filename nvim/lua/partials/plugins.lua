bac404e6da696d555c096b327784476364948572
Kristijan Husak <husakkristijan@gmail.com>
2024-01-30 21:14:03 +0100

    Temporarily switch to own fork of plenary

--- nvim/lua/partials/plugins.lua
+++ nvim/lua/partials/plugins.lua
@@ -3,7 +3,7 @@ return {
   { 'tpope/vim-sleuth', event = 'VeryLazy' },
   { 'tpope/vim-abolish', event = 'VeryLazy' },
   { 'tpope/vim-surround', event = 'VeryLazy' },
-  { 'nvim-lua/plenary.nvim' },
+  { 'kristijanhusak/plenary.nvim', branch = 'feat/luassert-types' },
   { 'stefandtw/quickfix-reflector.vim', event = 'VeryLazy' },
   { 'wakatime/vim-wakatime', event = 'VeryLazy' },
   { 'LunarVim/bigfile.nvim' },
