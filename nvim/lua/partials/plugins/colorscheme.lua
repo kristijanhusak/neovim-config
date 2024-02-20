ec01ab60dab1bfc1cfcdc50df3f368cfd357cd1e
Kristijan Husak <husakkristijan@gmail.com>
2024-02-20 11:46:20 +0100

    remove synmaxcol

--- nvim/lua/partials/plugins/colorscheme.lua
+++ nvim/lua/partials/plugins/colorscheme.lua
@@ -5,7 +5,6 @@ local colorscheme = {
 }
 colorscheme.config = function()
   vim.opt.background = vim.env.NVIM_COLORSCHEME_BG or 'dark'
-  vim.opt.synmaxcol = 300
 
   vim.cmd.filetype('plugin indent on')
   vim.cmd.syntax('on')
