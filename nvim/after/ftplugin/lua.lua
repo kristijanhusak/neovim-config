d913a0479560aed195b8859eff3154505da894ca
Kristijan Husak <husakkristijan@gmail.com>
2024-02-22 16:29:27 +0100

    Fix generating doc block for lua

--- nvim/after/ftplugin/lua.lua
+++ nvim/after/ftplugin/lua.lua
@@ -3,7 +3,7 @@ local fn = vim.fn
 local utils = require('partials.utils')
 
 local function do_print()
-  local view = fn.winsaveview()
+  local view = fn.winsaveview() or {}
   local word = fn.expand('<cword>')
   local scope = utils.get_gps_scope(word)
   if not scope:match(vim.pesc(word) .. '$') then
@@ -23,28 +23,43 @@ function lua.setup()
 end
 
 function lua.generate_docblock()
-  local ts_utils = require('nvim-treesitter.ts_utils')
-  local node = ts_utils.get_node_at_cursor()
-  if node:type() ~= 'identifier' then
-    while node and node:type() ~= 'function_name' do
-      node = node:parent()
-    end
-  end
-  if not node then
+  local node = vim.treesitter.get_node()
+  assert(node)
+  local is_method = node:type() == 'identifier' and node:parent():type() == 'method_index_expression'
+  local is_parameter = node:type() == 'identifier' and node:parent():type() == 'parameters'
+
+  if is_parameter then
+    node = node:parent():prev_named_sibling():field('method')[1]
+  elseif is_method then
+    node = node:parent():field('method')[1]
+  else
     return
   end
-  local param_node = ts_utils.get_next_node(node)
-  if not param_node or param_node:type() ~= 'parameters' then
-    return
-  end
+
+  local parameters_node = node:parent():next_named_sibling()
+  assert(parameters_node)
+  local method_name = vim.treesitter.get_node_text(node, 0)
+
   local content = {}
 
-  for _, child_node in ipairs(ts_utils.get_named_children(param_node)) do
-    local node_text = ts_utils.get_node_text(child_node)[1]
-    table.insert(content, string.format('---@param %s string', node_text))
+  if method_name:find('^_') then
+    table.insert(content, '---@private')
   end
 
-  table.insert(content, '---@return string')
+  local return_type = 'string'
+
+  if method_name:find('^_?is_') or method_name:find('^_?has_') then
+    return_type = 'boolean'
+  end
+
+  for child in parameters_node:iter_children() do
+    if child:type() == 'identifier' then
+      local node_text = vim.treesitter.get_node_text(child, 0)
+      table.insert(content, string.format('---@param %s string', node_text))
+    end
+  end
+
+  table.insert(content, '---@return ' .. return_type)
   fn.append(fn.line('.') - 1, content)
 end
 
