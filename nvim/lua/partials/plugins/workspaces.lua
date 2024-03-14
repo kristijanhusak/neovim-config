b3c471e25087ab3210eebbad2fb7e9b7f2df3c7c
Kristijan Husak <husakkristijan@gmail.com>
2024-03-14 16:31:30 +0100

    test-fzf
    Disable fzf header
    
    Fix fzf mappings and reduce preview window width

--- nvim/lua/partials/plugins/workspaces.lua
+++ nvim/lua/partials/plugins/workspaces.lua
@@ -1,16 +1,34 @@
 return {
   'natecraddock/workspaces.nvim',
   cmd = { 'WorkspacesAdd', 'WorkspacesOpen' },
-  opts = {
-    path = vim.fs.normalize('~/Dropbox/workspaces'),
-    hooks = {
-      open = function()
-        vim.notify((' Switched to project: %s '):format(vim.fn.getcwd()))
-        local local_nvimrc = vim.fn.getcwd() .. '/.nvimrc'
-        if vim.secure.read(local_nvimrc) then
-          vim.cmd.source(local_nvimrc)
-        end
-      end,
-    },
-  },
+  init = function()
+    vim.keymap.set('n', '<Leader>w', function()
+      local workspaces = {}
+      for _, workspace in ipairs(require('workspaces').get()) do
+        workspaces[workspace.path] = workspace.name
+      end
+      return require('fzf-lua').fzf_exec(vim.tbl_keys(workspaces), {
+        actions = {
+          ['default'] = function(selected)
+            vim.cmd(('WorkspacesOpen %s'):format(workspaces[selected[1]]))
+          end,
+        }
+      })
+    end)
+  end,
+  config = function()
+    local workspaces = require('workspaces')
+    workspaces.setup({
+      path = vim.fs.normalize('~/Dropbox/workspaces'),
+      hooks = {
+        open = function()
+          vim.notify((' Switched to project: %s '):format(vim.fn.getcwd()))
+          local local_nvimrc = vim.fn.getcwd() .. '/.nvimrc'
+          if vim.secure.read(local_nvimrc) then
+            vim.cmd.source(local_nvimrc)
+          end
+        end,
+      },
+    })
+  end,
 }
