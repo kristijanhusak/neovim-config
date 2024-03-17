4f785ba66318c686427d1d5440ea30b80be2b5e1
Kristijan Husak <husakkristijan@gmail.com>
2024-03-17 16:43:52 +0100

    Fix install for oh my zsh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ install_oh_my_zsh() {
   echo "Setting up zsh..." \
   && rm -rf ~/.zshrc \
   && ln -s $(pwd)/zsh/zshrc ~/.zshrc \
-  && git clone git://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions \
+  && git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions \
   && git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting \
   && git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k \
   && ln -s $(pwd)/zsh/themes/* ~/.oh-my-zsh/custom/themes \
