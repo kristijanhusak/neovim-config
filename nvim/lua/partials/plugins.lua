1ce631748e63561c917b2f6aaf570cec801a496b
Kristijan Husak <husakkristijan@gmail.com>
2024-04-22 16:02:18 +0200

    Go back to main plenary

--- nvim/lua/partials/plugins.lua
+++ nvim/lua/partials/plugins.lua
@@ -3,7 +3,7 @@ return {
   { 'tpope/vim-sleuth', event = 'VeryLazy' },
   { 'tpope/vim-abolish', event = 'VeryLazy' },
   { 'tpope/vim-surround', event = 'VeryLazy' },
-  { 'kristijanhusak/plenary.nvim', branch = 'feat/luassert-types' },
+  { 'nvim-lua/plenary.nvim' },
   { 'stefandtw/quickfix-reflector.vim', event = 'VeryLazy' },
   { 'wakatime/vim-wakatime', event = 'VeryLazy' },
   { 'LunarVim/bigfile.nvim', lazy = false, priority = 2000 },
