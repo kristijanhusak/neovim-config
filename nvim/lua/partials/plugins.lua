d46219c91fb5a6d75f25d5a5ab86c628a1fc0bbb
Kristijan Husak <husakkristijan@gmail.com>
2024-05-19 11:22:35 +0200

    test indentmini

--- nvim/lua/partials/plugins.lua
+++ nvim/lua/partials/plugins.lua
@@ -7,4 +7,15 @@ return {
   { 'stefandtw/quickfix-reflector.vim', event = 'VeryLazy' },
   { 'wakatime/vim-wakatime', event = 'VeryLazy' },
   { 'LunarVim/bigfile.nvim', lazy = false, priority = 2000 },
+  {
+    'nvimdev/indentmini.nvim',
+    event = 'VeryLazy',
+    config = function()
+      require('indentmini').setup({
+        char = '▏',
+      })
+      vim.cmd('hi! link IndentLine IndentBlanklineChar')
+      vim.cmd('hi! link IndentLineCurrent IndentBlanklineContextChar')
+    end,
+  },
 }
