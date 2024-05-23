0987d895980d58cacb087535d83a92daa0cdab8b
Kristijan Husak <husakkristijan@gmail.com>
2024-05-23 18:09:30 +0200

    fix configuration for nvim-pqf

--- nvim/lua/partials/plugins/pqf.lua
+++ nvim/lua/partials/plugins/pqf.lua
@@ -3,10 +3,10 @@ return {
   event = 'VeryLazy',
   opts = {
     signs = {
-      error = _G.kris.diagnostic_icons[vim.diagnostic.severity.ERROR],
-      warning = _G.kris.diagnostic_icons[vim.diagnostic.severity.WARN],
-      info = _G.kris.diagnostic_icons[vim.diagnostic.severity.INFO],
-      hint = _G.kris.diagnostic_icons[vim.diagnostic.severity.HINT],
+      error = { text = _G.kris.diagnostic_icons[vim.diagnostic.severity.ERROR] },
+      warning = { text = _G.kris.diagnostic_icons[vim.diagnostic.severity.WARN] },
+      info = { text = _G.kris.diagnostic_icons[vim.diagnostic.severity.INFO] },
+      hint = { text = _G.kris.diagnostic_icons[vim.diagnostic.severity.HINT] },
     },
   },
 }
