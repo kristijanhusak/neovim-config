7a10ed7d6269ae31d28ae54f411cb73d9b2fcca6
Kristijan Husak <husakkristijan@gmail.com>
2024-05-31 14:20:36 +0200

    Run indentmini only for version > 0.10

--- nvim/lua/partials/plugins/indentmini.lua
+++ nvim/lua/partials/plugins/indentmini.lua
@@ -1,6 +1,9 @@
 return {
   'nvimdev/indentmini.nvim',
   event = 'VeryLazy',
+  enabled = function()
+    return vim.fn.has('nvim-0.10') > 0
+  end,
   config = function()
     require('indentmini').setup({
       char = '▏',
