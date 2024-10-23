491948f9971b42d23c18c329fcc8c6cbc0d9e20e
Kristijan Husak <husakkristijan@gmail.com>
2024-10-23 20:36:14 +0200

    Test custom lsp completion

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,4 +1,6 @@
 _G.kris = {}
+-- Monkey patch lsp completion with modifications
+vim.lsp.completion = require('partials.lsp_completion')
 require('partials.abbreviations')
 require('partials.settings')
 require('partials.lazy')
