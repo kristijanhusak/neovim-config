ad978e7415490460c23273fc1b28d2d2505a4a70
Kristijan Husak <husakkristijan@gmail.com>
2024-10-25 09:59:48 +0200

    Remove custom lsp completion

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,6 +1,4 @@
 _G.kris = {}
--- Monkey patch lsp completion with modifications
-vim.lsp.completion = require('partials.lsp_completion')
 require('partials.abbreviations')
 require('partials.settings')
 require('partials.lazy')
