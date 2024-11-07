feaa0b7455e0dd2ed4e4d60ddf4d04e5bb352ddf
Kristijan Husak <husakkristijan@gmail.com>
2024-11-07 13:55:04 +0100

    Monkey patch vim.notify manually

--- nvim/lua/partials/plugins/snacks.lua
+++ nvim/lua/partials/plugins/snacks.lua
@@ -2,17 +2,6 @@ return {
   'folke/snacks.nvim',
   priority = 1000,
   lazy = false,
-  opts = {
-    bigfile = { enabled = true },
-    statuscolumn = { enabled = true },
-    words = { enabled = true, debounce = 100 },
-    notifier = {
-      enabled = true,
-      top_down = false,
-      margin = { bottom = 1 }
-    },
-    quickfile = { enabled = false },
-  },
   keys = {
     {
       '<leader>dl',
@@ -22,4 +11,30 @@ return {
       desc = 'Lazygit',
     },
   },
+  config = function()
+    require('snacks').setup({
+      bigfile = { enabled = true },
+      statuscolumn = { enabled = true },
+      words = { enabled = true, debounce = 100 },
+      quickfile = { enabled = false },
+      notifier = {
+        enabled = false,
+        top_down = false,
+        margin = { bottom = 1 },
+      },
+    })
+
+    -- Override the default notify function and make it work as nvim-notify
+    vim.notify = function(msg, level, o)
+      o = o or {}
+      if o.replace then
+        o.id = o.replace
+      end
+      local result = Snacks.notifier.notify(msg, level, o)
+      if type(result) == 'table' then
+        return result
+      end
+      return { id = result }
+    end
+  end,
 }
