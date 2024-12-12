ef0c7797bdf13ca57c9ec65e171644c7d9d53e43
Kristijan Husak <husakkristijan@gmail.com>
2024-12-12 18:24:04 +0100

    cleanup some internal plugins

--- nvim/after/plugin/replace_pair.lua
+++ nvim/after/plugin/replace_pair.lua
@@ -15,25 +15,24 @@ function replace_pair.run()
   end
 
   local char = fn.getline('.'):sub(fn.col('.'), fn.col('.'))
-  if chars[char] then
-    local new_char = fn.nr2char(fn.getchar())
-    local flags = 'nW'
-    local search_char = char
-    if chars[char][2] == 'right' then
-      flags = flags .. 'b'
-      search_char = chars[char][1]
-    end
-    local pos = fn.searchpairpos(fn.escape(search_char, '[]'), '', fn.escape(chars[search_char][1], '[]'), flags)
-    local lnum = pos[1]
-    local col = pos[2]
-    vim.cmd('norm!r' .. new_char)
-    local view = fn.winsaveview()
-    vim.fn.cursor({ lnum, col })
-    vim.cmd('norm!r' .. chars[new_char][1])
-    return fn.winrestview(view)
+  if not chars[char] then
+    return fn.feedkeys('R', 'n')
   end
 
-  return fn.feedkeys('R', 'n')
+  local new_char = fn.nr2char(fn.getchar())
+  local flags = 'nW'
+  local search_char = char
+  if chars[char][2] == 'right' then
+    flags = flags .. 'b'
+    search_char = chars[char][1]
+  end
+  local lnum, col =
+    unpack(fn.searchpairpos(fn.escape(search_char, '[]'), '', fn.escape(chars[search_char][1], '[]'), flags))
+  vim.cmd('norm!r' .. new_char)
+  local view = fn.winsaveview()
+  vim.fn.cursor({ lnum, col })
+  vim.cmd('norm!r' .. chars[new_char][1])
+  return fn.winrestview(view)
 end
 
 vim.keymap.set('n', 'R', replace_pair.run, { silent = true })
--- nvim/after/plugin/ui.lua
+++ nvim/after/plugin/ui.lua
@@ -1,12 +1,11 @@
 local utils = require('partials.utils')
-local ui = {
-  last_input = {},
-}
+local ui = {}
 
 local function get_win_width(value_length, opts)
   return math.max(value_length + 10, (opts.prompt and opts.prompt:len() + 10 or 0))
 end
 
+---@diagnostic disable-next-line: duplicate-set-field
 vim.ui.input = function(opts, on_confirm)
   vim.validate({
     on_confirm = { on_confirm, 'function', false },
@@ -16,41 +15,34 @@ vim.ui.input = function(opts, on_confirm)
   local win_width = get_win_width(current_val:len(), opts)
   local eventignore = vim.opt.eventignore:get()
   vim.opt.eventignore:append('WinLeave')
+
   local bufnr, winnr = vim.lsp.util.open_floating_preview({ current_val }, '', {
     height = 1,
     border = 'rounded',
     width = win_width,
     wrap = false,
-    title = opts.prompt,
+    title = ' ' .. opts.prompt,
     title_pos = 'center',
   })
 
-  ui.last_input = {
-    val = current_val,
-    on_confirm = on_confirm,
-  }
   vim.api.nvim_win_set_config(winnr, { width = win_width })
   vim.api.nvim_set_current_win(winnr)
   vim.api.nvim_set_option_value('modifiable', true, { buf = bufnr })
   vim.api.nvim_set_option_value('sidescrolloff', 0, { win = winnr })
   vim.b[bufnr].disable_blink = true
 
-  vim.keymap.set('i', '<CR>', ui.on_input, { buffer = bufnr })
+  vim.keymap.set('i', '<CR>', function()
+    local input = vim.trim(vim.fn.getline('.'))
+    vim.api.nvim_win_close(0, true)
+    vim.api.nvim_feedkeys(utils.esc('<Esc>'), 'i', true)
+
+    return on_confirm(#input > 0 and input or nil)
+  end, { buffer = bufnr })
+
   vim.defer_fn(function()
     vim.cmd.startinsert({ bang = true })
     vim.opt.eventignore = eventignore
   end, 10)
-end
-
-function ui.on_input()
-  local input = vim.trim(vim.fn.getline('.'))
-  vim.api.nvim_win_close(0, true)
-  vim.api.nvim_feedkeys(utils.esc('<Esc>'), 'i', true)
-
-  if #input > 0 then
-    return ui.last_input.on_confirm(input)
-  end
-  return ui.last_input.on_confirm(nil)
 end
 
 return ui
