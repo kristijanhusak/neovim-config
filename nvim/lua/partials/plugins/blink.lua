7323b16eff3ae91efce230b6254edaa9963004bf
Kristijan Husak <husakkristijan@gmail.com>
2024-12-21 08:59:12 +0100

    Fix blink cmp configuration

--- nvim/lua/partials/plugins/blink.lua
+++ nvim/lua/partials/plugins/blink.lua
@@ -7,32 +7,28 @@ return {
       return not (vim.bo.buftype == 'prompt' or vim.b.disable_blink)
     end,
     sources = {
-      completion = {
-        enabled_providers = function()
-          if vim.bo.filetype == 'sql' then
-            return { 'snippets', 'dadbod' }
-          end
+      default = function()
+        if vim.bo.filetype == 'sql' then
+          return { 'snippets', 'dadbod' }
+        end
 
-          if vim.bo.filetype == 'org' then
-            return { 'orgmode', 'buffer' }
-          end
+        if vim.bo.filetype == 'org' then
+          return { 'orgmode', 'buffer' }
+        end
 
-          return { 'lsp', 'snippets', 'path', 'buffer' }
-        end,
-      },
+        return { 'lsp', 'snippets', 'path', 'buffer' }
+      end,
       providers = {
         dadbod = {
           name = 'Dadbod',
           module = 'vim_dadbod_completion.blink',
+          fallbacks = { 'buffer' },
         },
         orgmode = {
           name = 'Orgmode',
           module = 'orgmode.org.autocompletion.blink',
+          fallbacks = { 'buffer' },
         },
-        buffer = {
-          fallback_for = { 'lsp', 'dadbod', 'orgmode' },
-        },
-
         snippets = {
           score_offset = -5,
           enabled = function()
