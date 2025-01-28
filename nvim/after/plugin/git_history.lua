fafb3c94a495a90bd7e485bab3329d07c44b59de
Kristijan Husak <husakkristijan@gmail.com>
2025-01-28 20:45:22 +0100

    Tweak history saver to work only in own cwd

--- nvim/after/plugin/git_history.lua
+++ nvim/after/plugin/git_history.lua
@@ -5,27 +5,33 @@ local function save_to_git_history()
   if vim.bo.buftype ~= '' then
     return
   end
-  local git_dir = uv.cwd() .. '/.git'
+  local cwd = uv.cwd() or ''
+  local git_dir = cwd .. '/.git'
   local is_git = uv.fs_stat(git_dir)
+  if not uv.fs_stat(git_dir) then
+    cwd = vim.fs.root(cwd, '.git')
+    git_dir = cwd .. '/.git'
+  end
+  is_git = uv.fs_stat(git_dir)
   if not is_git or is_git.type ~= 'directory' then
     return
   end
-  local filepath = vim.api.nvim_buf_get_name(0)
-  local path = ('%s/history%s'):format(git_dir, filepath)
+  local filepath = vim.fn.expand('%:p')
+  if not vim.startswith(filepath, cwd) then
+    return
+  end
+  local path = ('%s/history%s'):format(git_dir, filepath:sub(#cwd + 1))
   local file_dir = vim.fs.dirname(path)
   local has_dir = uv.fs_stat(file_dir)
   if not has_dir or has_dir.type ~= 'directory' then
     vim.fn.mkdir(file_dir, 'p')
   end
-  vim.fn.jobstart({ 'cp', filepath, path }, {
-    on_stderr = function(_, data)
-      if data and data[1] and data[1] ~= '' then
-        vim.notify('Error saving file to git history: ' .. data[1], vim.log.levels.ERROR, {
-          title = 'Git History',
-        })
-      end
-    end,
-  })
+  local result = vim.system({ 'cp', filepath, path }):wait()
+  if result.stderr and result.stderr ~= '' then
+    vim.notify('Error saving file to git history: ' .. result.stderr, vim.log.levels.ERROR, {
+      title = 'Git History',
+    })
+  end
 end
 
 vim.api.nvim_create_autocmd('BufWritePost', {
