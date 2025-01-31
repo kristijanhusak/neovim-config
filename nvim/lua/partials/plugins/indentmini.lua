29c2f600f6da1a8ad42da6c9e229ada6c13a64b3
Kristijan Husak <husakkristijan@gmail.com>
2025-01-31 15:33:22 +0100

    Remove check for nvim 0.10+ for indentmini

--- nvim/lua/partials/plugins/indentmini.lua
+++ nvim/lua/partials/plugins/indentmini.lua
@@ -1,13 +1,8 @@
 return {
   'nvimdev/indentmini.nvim',
   event = 'VeryLazy',
-  enabled = function()
-    return vim.fn.has('nvim-0.10') > 0
-  end,
-  config = function()
-    require('indentmini').setup({
-      char = '▏',
-      exclude = { 'dbout' },
-    })
-  end,
+  opts = {
+    char = '▏',
+    exclude = { 'dbout' },
+  },
 }
