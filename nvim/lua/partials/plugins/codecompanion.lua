a84dd777a3c202df880c075f89644b919f4efacb
Kristijan Husak <husakkristijan@gmail.com>
2025-08-08 09:49:02 +0200

    Add codecompanion progress notification

--- nvim/lua/partials/plugins/codecompanion.lua
+++ nvim/lua/partials/plugins/codecompanion.lua
@@ -22,27 +22,44 @@ return {
       desc = 'Open Code Companion Chat',
     },
   },
-  opts = {
-    extensions = {
-      history = {
-        enabled = true,
-        opts = {
-          picker = 'default',
+  config = function()
+    require('codecompanion').setup({
+      extensions = {
+        history = {
+          enabled = true,
+          opts = {
+            picker = 'default',
+          },
         },
       },
-    },
-    strategies = {
-      chat = { adapter = 'copilot' },
-      inline = { adapter = 'copilot' },
-    },
-    adapters = {
-      gemini = function()
-        return require('codecompanion.adapters').extend('gemini', {
-          env = {
-            api_key = vim.env.GEMINI_API_KEY,
-          },
-        })
+      strategies = {
+        chat = { adapter = 'copilot' },
+        inline = { adapter = 'copilot' },
+      },
+      adapters = {
+        gemini = function()
+          return require('codecompanion.adapters').extend('gemini', {
+            env = {
+              api_key = vim.env.GEMINI_API_KEY,
+            },
+          })
+        end,
+      },
+    })
+
+    vim.api.nvim_create_autocmd({ 'User' }, {
+      pattern = 'CodeCompanionRequest*',
+      group = vim.api.nvim_create_augroup('CodeCompanionStatus', { clear = true }),
+      callback = function(request)
+        if request.match == 'CodeCompanionRequestStarted' then
+          Snacks.notify.info('  CodeCompanion Running...', {
+            id = 'codecompanion_status',
+            timeout = 0,
+          })
+        elseif request.match == 'CodeCompanionRequestFinished' then
+          Snacks.notifier.hide('codecompanion_status')
+        end
       end,
-    },
-  },
+    })
+  end,
 }
