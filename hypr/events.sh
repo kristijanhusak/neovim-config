b830277ef837c309f68455a9de23281e3ce42e45
Kristijan Husak <husakkristijan@gmail.com>
2026-06-15 21:12:39 +0200

    Fix noctalia plugins

--- hypr/events.sh
+++ hypr/events.sh
@@ -7,7 +7,7 @@ handle() {
 }
 
 handle_submap() {
-  noctalia msg scripted-widget submap all refresh
+  noctalia msg plugin me/submap:submap all refresh
 }
 
 socat -U - UNIX-CONNECT:$XDG_RUNTIME_DIR/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock | while read -r line; do handle "$line"; done
