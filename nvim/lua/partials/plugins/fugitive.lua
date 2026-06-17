3f470a93f89783935816c3691e0c84e988e39b90
Kristijan Husak <husakkristijan@gmail.com>
2026-06-17 15:06:32 +0200

    Remove diffview and use fugitive for file history

--- nvim/lua/partials/plugins/fugitive.lua
+++ nvim/lua/partials/plugins/fugitive.lua
@@ -1,4 +1,22 @@
 vim.keymap.set('n', '<Leader>gf', ':vert G<CR>', { silent = true, desc = 'Open fugitive' })
+vim.keymap.set('n', '<leader>gc', function()
+  vim.cmd('botright Git commit')
+end, { silent = true, desc = 'Git commit' })
+vim.keymap.set('n', '<leader>ga', function()
+  vim.cmd('botright Git commit --amend')
+end, { silent = true, desc = 'Git commit --amend' })
+vim.keymap.set('n', '<leader>gp', ':Git push<CR>', { silent = true, desc = 'Git push' })
+vim.keymap.set(
+  'n',
+  '<leader>gF',
+  ':Git push --force-with-lease<CR>',
+  { silent = true, desc = 'Git push --force-with-lease' }
+)
+
+vim.keymap.set('n', '<leader>gd', function()
+  vim.cmd.tabnew(vim.api.nvim_buf_get_name(0))
+  vim.cmd('0Gclog')
+end, { silent = true, desc = 'Show file git history' })
 
 local git_group = vim.api.nvim_create_augroup('custom_fugitive', { clear = true })
 vim.api.nvim_create_autocmd('FileType', {
