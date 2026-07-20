9da810c3996caa2eabf14fb525e7638b43985875
Kristijan Husak <husakkristijan@gmail.com>
2026-07-20 09:57:24 +0200

    Replace arborist with nvim-treesitter

--- nvim/lua/partials/plugins/treesitter.lua
+++ nvim/lua/partials/plugins/treesitter.lua
@@ -4,7 +4,6 @@ return {
   dependencies = {
     { 'nvim-treesitter/nvim-treesitter-textobjects' },
   },
-  lazy = false,
   config = function()
     require('nvim-treesitter').setup()
     require('nvim-treesitter-textobjects').setup()
@@ -13,38 +12,13 @@ return {
     end, require('nvim-treesitter').get_available(2))
     require('nvim-treesitter').install(valid_parsers)
     vim.api.nvim_create_autocmd('FileType', {
-      pattern = {
-        'c',
-        'cpp',
-        'diff',
-        'dockerfile',
-        'gitdiff',
-        'gitignore',
-        'go',
-        'graphql',
-        'javascript',
-        'javascriptreact',
-        'lua',
-        'markdown',
-        'mysql',
-        'pgsql',
-        'php',
-        'python',
-        'ruby',
-        'sh',
-        'sql',
-        'terraform',
-        'toml',
-        'typescript',
-        'typescriptreact',
-        'vim',
-        'yaml',
-        'http',
-        'codecompanion',
-      },
+      pattern = '*',
       group = vim.api.nvim_create_augroup('nvim-treesitter-fts', { clear = true }),
       callback = function(args)
-        vim.treesitter.start(args.buf)
+        local parser = vim.treesitter.get_parser(args.buf, vim.bo[args.buf].filetype)
+        if parser then
+          vim.treesitter.start(args.buf)
+        end
       end,
     })
 
