557ddb4c89807c654a49a17a45b3117c94c4c597
Kristijan Husak <husakkristijan@gmail.com>
2026-08-14 11:40:55 +0200

    Remove Go custom format function

--- nvim/after/ftplugin/go.lua
+++ nvim/after/ftplugin/go.lua
@@ -15,27 +15,5 @@ function go.add_tags()
   vim.fn.winrestview(view)
 end
 
-function go.format()
-  local params = vim.lsp.util.make_range_params()
-  params.context = { only = { 'source.organizeImports' } }
-  local result = vim.lsp.buf_request_sync(0, 'textDocument/codeAction', params)
-  for cid, res in pairs(result or {}) do
-    for _, r in pairs(res.result or {}) do
-      if r.edit then
-        local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or 'utf-16'
-        vim.lsp.util.apply_workspace_edit(r.edit, enc)
-      end
-    end
-  end
-  vim.lsp.buf.format({ async = false })
-end
-
-local go_group = vim.api.nvim_create_augroup('vimrc_go', { clear = true })
-vim.api.nvim_create_autocmd('BufWritePre', {
-  pattern = '*.go',
-  callback = go.format,
-  group = go_group,
-})
-
 vim.api.nvim_create_user_command('GoAddTags', go.add_tags, { force = true })
 go.setup()
